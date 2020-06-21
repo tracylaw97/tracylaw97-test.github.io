e36f781510afea4710dbb435c5391956eb5535c8
tracylaw97 <64721765+tracylaw97@users.noreply.github.com>
2020-06-21 16:48:44 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,86 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-room-2",
+      "name": "Panorama Room 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5925364500386454,
+        "pitch": -0.06657276405443824,
+        "fov": 1.2809123973965084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8891104478038914,
+          "pitch": 0.3936098083882946,
+          "rotation": 0,
+          "target": "0-panorama-room-2"
+        },
+        {
+          "yaw": 1.0749349349768238,
+          "pitch": 0.3517916480607379,
+          "rotation": 0,
+          "target": "1-panorama-room-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama-room-1",
+      "name": "Panorama Room 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SDF Test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
